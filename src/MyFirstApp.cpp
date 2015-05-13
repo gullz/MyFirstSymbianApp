9465376c19fa5b0a1cc9d9b6240f12db45604fef
gullz <bpulathisi@hotmail.com>
2015-05-13 16:24:42 +0530

    Initial.1

--- src/MyFirstApp.cpp
+++ src/MyFirstApp.cpp
@@ -28,59 +28,18 @@ _LIT(KTextPressAnyKey, " [press any key]\n");
 
 //  Global Variables
 
-LOCAL_D CConsoleBase* console; // write all messages to this
+LOCAL_D CConsoleBase* console; 
 
-
-//  Local Functions
-
-LOCAL_C void MainL()
-	{
-	//
-	// add your program code here, example code below
-	//
-	console->Write(_L("Hello, world!\n"));
-	}
-
-LOCAL_C void DoStartL()
-	{
-	// Create active scheduler (to run active objects)
-	CActiveScheduler* scheduler = new (ELeave) CActiveScheduler();
-	CleanupStack::PushL(scheduler);
-	CActiveScheduler::Install(scheduler);
-
-	MainL();
-
-	// Delete active scheduler
-	CleanupStack::PopAndDestroy(scheduler);
-	}
-
-//  Global Functions
 
 GLDEF_C TInt E32Main()
 	{
-	// Create cleanup stack
-	__UHEAP_MARK;
-	CTrapCleanup* cleanup = CTrapCleanup::New();
-
 	// Create output console
-	TRAPD(createError, console = Console::NewL(KTextConsoleTitle, TSize(
-			KConsFullScreen, KConsFullScreen)));
-	if (createError)
-		{
-		delete cleanup;
-		return createError;
-		}
-
-	// Run application code inside TRAP harness, wait keypress when terminated
-	TRAPD(mainError, DoStartL());
-	if (mainError)
-		console->Printf(KTextFailed, mainError);
+	console = Console::NewL(KTextConsoleTitle, TSize(KConsFullScreen, KConsFullScreen));
+	console->Write(_L("Hello, world!\n"));
 	console->Printf(KTextPressAnyKey);
 	console->Getch();
 
 	delete console;
-	delete cleanup;
-	__UHEAP_MARKEND;
 	return KErrNone;
 	}
 
